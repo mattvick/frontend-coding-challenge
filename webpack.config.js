0a5e09e1e8fd3033391f04cf9dfbc3335c4de97e
Matthew <vickery.matthew@gmail.com>
2021-06-15 13:42:31 +0200

    Store the Movie DB API key in .env file and git ignore this

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const Dotenv = require('dotenv-webpack');
 
 module.exports = {
   entry: path.join(__dirname, 'src', 'index.js'),
@@ -45,5 +46,6 @@ module.exports = {
     new HtmlWebpackPlugin({
       template: path.join(__dirname, 'src', 'index.html'),
     }),
+    new Dotenv(),
   ],
 };
