65c07920966c5f28265257c1237b87725986e034
Matthew <vickery.matthew@gmail.com>
2021-06-15 17:53:31 +0200

    Add routes

--- src/index.js
+++ src/index.js
@@ -1,13 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from 'react-router-dom';
 
+import Movie from './Movie';
 import Search from './Search';
 
 const App = () => (
-  <>
-    <h1>Frontend Coding Challenge</h1>
-    <Search />
-  </>
+  <Router>
+    <div className="container">
+      <header>
+        <h1>Frontend Coding Challenge</h1>
+        <Search />
+      </header>
+      <main>
+        <Switch>
+          <Route path="/movie/:id">
+            <Movie />
+          </Route>
+          <Route>
+            <p>Search above to find information about your favourite movies.</p>
+          </Route>
+        </Switch>
+      </main>
+    </div>
+  </Router>
 );
 
 ReactDOM.render(<App />, document.getElementById('root'));
